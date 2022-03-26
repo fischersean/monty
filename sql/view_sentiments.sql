3210fe541a90548b184cf88c65cf93e34ee12d4f
Sean Fischer <seanwfischer@gmail.com>
2022-03-26 18:25:07 -0400

    more exploring

--- sql/view_sentiments.sql
+++ sql/view_sentiments.sql
@@ -1,10 +1,8 @@
 SELECT 
     g.name,
     g.run_date,
-    g.run_hour,
-    g.toxic_index,
-    g.stddev_toxic_index,
-    g.stddev_toxic_index / g.toxic_index AS stdev_percent
+    AVG(g.toxic_index) AS avg_toxic_index,
+    MAX(g.stddev_toxic_index) AS stddev_toxic_index
 FROM (
     SELECT
         f.name,
@@ -13,11 +11,11 @@ FROM (
         f.toxic_index,
         stddev(f.toxic_index) OVER (
             PARTITION BY f.name, f.run_date
-        ) as stddev_toxic_index
+        ) AS stddev_toxic_index
     FROM (
         SELECT
             subreddits.name,
-            watermarks.run_start::DATE as run_date,
+            watermarks.run_start::DATE AS run_date,
             EXTRACT(HOUR FROM watermarks.run_start) as run_hour,
             count_comments,
             count_posts,
@@ -26,7 +24,8 @@ FROM (
             score_compound_mean - score_compound_weighted_mean AS toxic_index
         FROM sentiment
         LEFT JOIN subreddits ON sentiment.subreddit_id=subreddits.id
-        LEFT JOIN watermarks on sentiment.run_id=watermarks.id
+        LEFT JOIN watermarks ON sentiment.run_id=watermarks.id
     ) AS f
 ) as g
-ORDER BY g.name, g.run_date ASC, g.run_hour ASC;
+GROUP BY g.name, g.run_date
+ORDER BY avg_toxic_index DESC;
